ac3c039c7668c4c455c2e3513beb0dac2047afb1
Bek1m <12bekim21@gmail.com>
2024-07-30 17:23:39 +0200

    Create Home page, list pages for author and books, and details pages for authors and books

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+
+    num_authors = Author.objects.count()
+    search_word = "Song"
+    search_word2 = "Fantasy"
+
+    num_of_genres_containing_word = Genre.objects.filter(name__exact=search_word2).count()
+    num_books_containing_word = Book.objects.filter(title__icontains=search_word).count()
+    
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_books_containing_word':num_books_containing_word,
+        'num_of_genres_containing_word':num_of_genres_containing_word,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
