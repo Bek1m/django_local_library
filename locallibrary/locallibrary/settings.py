f74d25af4bfb5b770e004b68d68461da1c8c85e9
Bekim <66366302+Bek1m@users.noreply.github.com>
2024-08-07 14:20:33 +0200

    Update settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=@)jreajuqit1=1@gn$12v%3ck%xg_p39sf3k54v)4nstch-0q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bekim.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
